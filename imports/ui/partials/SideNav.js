def0abb940d3e74cd27c44cb9e1164b0b1b4aef7
Hiroto Shioi <shioihi@me.com>
2016-07-07 06:11:34 +0900

    changed the design of signin/out link in the sidenav

--- imports/ui/partials/SideNav.js
+++ imports/ui/partials/SideNav.js
@@ -2,3 +2,9 @@ import { Template } from 'meteor/templating';
 
 import './SideNav.html';
 import './NewMemoModal.js';
+
+Template.SideNav.events({
+	'click .logout':()=>{
+		Meteor.logout();
+	}
+});
