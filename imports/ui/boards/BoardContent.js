df724ea954fd73922c91cdf4aa8337805cccbb65
Hiroto Shioi <shioihi@me.com>
2016-07-08 17:52:28 +0900

    fixed bug where no status showed edit icons

--- imports/ui/boards/BoardContent.js
+++ imports/ui/boards/BoardContent.js
@@ -13,8 +13,9 @@ Template.BoardContent.onCreated(function(){
 
 Template.BoardContent.helpers({
 	//need to use function() because of the scope problem (cannot use this_id);
+	//if status has id, edit mode is available
 	editMode:function(){
-		return Session.get("editMode") === this._id;
+		return Session.get("editMode") === this._id && this._id;
 	}
 });
 
