4daf059fc3d9ed7bf8d6b23d9bdb1054a4bbee22
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-11-10 17:45:03 +0900

    g5 - added spin animation while loading Memo
    When adding memo, we needed some kind of animation for user to notice
    that add memo action is in progress.

--- imports/ui/partials/NewMemoModal.js
+++ imports/ui/partials/NewMemoModal.js
@@ -18,15 +18,22 @@ const hooksObject = {
 	before:{
 		insert:function(doc){
 			let isValidForm = AutoForm.validateForm("NewMemo");
+			Session.set('isLoadingMemo',true);
 			if(isValidForm){
-				Meteor.call("addMemo",doc);
 				this.resetForm();
 				$('#afModal').closeModal();
+				Meteor.call("addMemo",doc,(err,result)=>{
+					if(!err){
+						Session.set('isLoadingMemo', false);					
+					}
+				});
 			}
 		}
 	},
 }
-
+Meteor.Spinner.options = {
+	color:"#fff"
+};
 AutoForm.hooks({
   NewMemo: hooksObject
 });
