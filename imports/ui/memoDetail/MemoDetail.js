d5ca6e73eff33129c3f3c4c4da974be3d00d49ed
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-11-14 15:13:57 +0900

    g25 - Detail link opens tab
    Description
    We want to open the new tab to the link when clicked.
    
    Solution
    To implement this, there are some things that are done.
    1. Used state to store memo record so it can be called from helpers
    and events
    2. Used the same open tab implementation as memo image
    opening link.

--- imports/ui/memoDetail/MemoDetail.js
+++ imports/ui/memoDetail/MemoDetail.js
@@ -3,17 +3,28 @@ import { TemplateController } from 'meteor/space:template-controller';
 import { Memos } from '../../api/memos.js';
 
 TemplateController('MemoDetail',{
+	state:{
+		memos: '',
+	},
+
 	onCreated(){
 	 	this.autorun(()=>{
 	 		this.subscribe('singleMemo',this.data._id);
+	 		this.state.memos = Memos.findOne({_id:this.data._id});
 	 	});
 	 	Session.set('Title',{name:"Detail"});
 	},
 
 	helpers:{
 		memo(){
-			const id = this.data._id;
-			return Memos.findOne({_id:id});
+			return this.state.memos;
 		},
+	},
+
+	events:{
+		'click .memo-detail-url'(){
+			window.open(this.state.memos.url,'_blank');
+			return false;
+		}
 	},
 });
