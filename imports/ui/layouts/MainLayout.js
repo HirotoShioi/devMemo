a7e0bd6293034d49145f5788c40d2e8cd218b42a
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-11-25 22:50:34 +0900

    g43 - Fix logic part2

--- imports/ui/layouts/MainLayout.js
+++ imports/ui/layouts/MainLayout.js
@@ -20,7 +20,7 @@ TemplateController('MainLayout',{
 				Session.set('isShrinkedSideNavShown',false);
 				Session.set('isSearchNavShown',false);
 			}
-			if(rwindow.$width() <= 992 || Meteor.userId()){
+			if(rwindow.$width() <= 992 && Meteor.userId() === true){
 				Session.set('isSearching',false);
 				return false;
 			}else{
