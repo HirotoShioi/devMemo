ab5caa664c80976dcde6fb77aefd2732caa70bdd
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-12-02 16:38:17 +0900

    g61 - Add time progress color
    Description
    Clock icon color now indicates expiration of the memos.

--- imports/ui/partials/List/SingleList.js
+++ imports/ui/partials/List/SingleList.js
@@ -1,8 +1,14 @@
 import './SingleList.html';
 import { TemplateController } from 'meteor/space:template-controller';
 import { Label } from '../../../api/label.js'
+import { moment } from 'meteor/momentjs:moment';
 
 TemplateController('SingleList',{
+	state:{
+		progressBarColor:'over-75',
+		progressRate:0,
+	},
+
 	onCreated(){
 		this.autorun(()=>{
 			this.subscribe('label');
@@ -18,6 +24,34 @@ TemplateController('SingleList',{
 		},
 		shouldNotify(){
 			return !this.data.notifiedToUser;
+		},
+		progressBarColor(){
+			const expireDate = moment(this.data.expiredAt);
+			const today = moment().format();
+			const progress = expireDate.diff(today, 'hours');
+			const expireLimit = this.data.expireIn*24;
+			let progressRate = this.state.progressRate;
+			progressRate = Math.floor((progress / expireLimit) * 100);
+
+			if(progressRate > 100){
+				progressRate = 100;
+			}
+			if(progressRate <= 0){
+				progressRate = 0;
+				this.state.isMemoExpired = true;
+			}
+
+			if(progressRate >= 75){
+				this.state.progressBarColor = 'over-75';
+			}else if(progressRate >= 50){
+				this.state.progressBarColor = 'over-50';
+			}else if(progressRate >= 25){
+				this.state.progressBarColor = 'over-25';
+			}else{
+				this.state.progressBarColor = 'over-0';
+			}
+
+			return this.state.progressBarColor;
 		},
 	},
 
