ff8b3f93482b5b80ed46e3608aaa50adabf31bb8
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-12-09 02:02:33 +0900

    g75 - Fix sidebar opacity, reset modal rework
    Description
    Removed opacity from sidebar, refactored reset modal helper.

--- imports/ui/partials/Modals/modalHelper.js
+++ imports/ui/partials/Modals/modalHelper.js
@@ -1,8 +1,10 @@
 export const resetModalForm = function(){
 	Session.set('showModal',false);
 	Session.set('modalFormType',null);
+	Session.set('modalFormType',null);
+	//ids
 	Session.set('deleteLabelId',null);
 	Session.set('addMemoSelectedLabelId',null);
-	Session.set('modalFormType',null);
 	Session.set('editLabelId', null);
+	Session.set('editMemoLabelId', null);
 };
