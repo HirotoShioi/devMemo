128b71ebe3a692a243b1c6a95ef492b647dd020f
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-12-15 11:02:26 +0900

    g76 - Add Loading PageTitle template
    Description
    Add Loading and PageTitle template

--- imports/ui/featured/Featured.js
+++ imports/ui/featured/Featured.js
@@ -1,6 +1,9 @@
 import './Featured.html';
 import { TemplateController } from 'meteor/space:template-controller';
 
+import '../partials/Loading.js';
+import '../layouts/component/PageTitle.js';
+
 TemplateController('Featured',{
 	onCreated(){
 		Session.set('Title',{name:"Featured"});
