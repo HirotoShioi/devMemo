7f1cc10dbdd0dcad8e7d865a7e358e7824f04849
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-12-18 23:01:46 +0900

    g105 - Add label
    Description
    Added label for memo modal

--- imports/ui/partials/Modals/MemoDetailModal.js
+++ imports/ui/partials/Modals/MemoDetailModal.js
@@ -7,6 +7,7 @@ import './MemoDetailModal.html';
 TemplateController('MemoDetailModal', {
   state: {
     memo: {},
+    label: {},
   },
   onCreated() {
     const self = this;
@@ -18,13 +19,20 @@ TemplateController('MemoDetailModal', {
     memo() {
       let memo = Memos.findOne({_id: Session.get('MemoDetailId')});
       this.state.memo = memo;
+      if (memo) {
+        this.state.label = memo.label();
+      }
       return memo;
     },
     truncate() {
-      if (this.state.memo.desc.length > 100) {
-        this.state.memo.desc = `${this.state.memo.desc.substring(0, 100)}...`;
+      if (this.state.memo) {
+        if (this.state.memo.desc.length > 100) {
+          this.state.memo.desc = `${this.state.memo.desc.substring(0, 100)}...`;
+        }
+        return this.state.memo.desc;
+      } else {
+        return false;
       }
-      return this.state.memo.desc;
     }
   },
 });
