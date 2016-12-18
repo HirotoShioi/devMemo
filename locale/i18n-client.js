80ff2d44971dcd3fe4ae934a5196e626320d0501
Hiroto Shioi <hiroto.shioi@iohk.io>
2016-12-18 12:52:52 +0900

    g52 - Add translation package
    Description
    Add translation package, add translations to client side view in English

---
+++ locale/i18n-client.js
@@ -0,0 +1,42 @@
+i18n.map('en', {
+  feature: {
+    favorites: 'Favorites',
+    recentlyViewed: 'Recently Viewed',
+    forYou: 'For You',
+  },
+
+  memo: {
+    hideExpired: 'Hide Expired',
+  },
+
+  search: {
+    search: 'Search',
+    searchInput: 'SearchInput',
+    frequentlyUsed: 'Frequently Used',
+    searchResult: 'Search result',
+    noResultsFound: 'No results found',
+    more: 'more',
+  },
+
+  label: {
+    label: 'Label',
+  },
+
+  sideNav: {
+    addMemo: 'addMemo',
+    featured: 'Featured',
+    memos: 'Memos',
+    search: 'Search',
+    labels: 'Labels',
+    about: 'About',
+  },
+
+  accounts: {
+    signIn: 'Sign in',
+    signOut: 'Sign out',
+  }
+});
+
+i18n.map('ja', {
+
+});
