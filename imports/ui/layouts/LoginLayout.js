997905b3988452a2a94c221ea4aca2fec5958651
Hiroto Shioi <hiroto.shioi@iohk.io>
2017-01-21 13:33:42 +0900

    g131 - Change application name to iine!!
    Description
    Change application name to iine!!

--- imports/ui/layouts/LoginLayout.js
+++ imports/ui/layouts/LoginLayout.js
@@ -12,6 +12,6 @@ import './LoginLayout.html';
 TemplateController('LoginLayout', {
   onCreated() {
     Session.set('shouldHeaderBeShownAtFullWindow', false);
-    Session.set('Title', {name: "Memoli"});
+    Session.set('Title', {name: "iine!!"});
   },
 });
